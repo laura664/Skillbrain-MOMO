64ea234750905f1c93a0b56814af125666f9902b
Laura <laura.andreea.vrabie1234@gmail.com>
2024-11-11 21:07:45 +0200

    Added sprint7

---
+++ sprint7-Js intro/tema2.js
@@ -0,0 +1,6 @@
+//Defifinim functia
+function tellFortune(N, Z, Y, X) {
+    return `Vei fi un ${X} in ${Y}, casatorit cu ${Z} si vei avea ${N} copii.`
+}
+
+console.log(tellFortune("3", "Emanuel", "Italia", "Programator"));
---
+++ sprint7-Js intro/tema3.js
@@ -0,0 +1,35 @@
+function theWeekDay(n) {
+    let text;
+    switch (n) {
+        case 1:
+            return "Luni";
+        case 2:
+            return "Marti";
+        case 3:
+            return "Miercuri";
+        case 4:
+            return "Joi";
+        case 5:
+            return "Vineri";
+        case 6:
+            return "Sambata";
+        case 7:
+            return "Duminica";
+        default:
+            return "necunoscut";
+    }
+
+    return text;
+}
+
+let dayNumber;
+let dayString;
+
+nr = 4
+text = theWeekDay(nr);
+
+function theWeekDay(dayNumber) {
+    const dayString = theWeekDay(day)
+}
+
+console.log(theWeekDay(n));
